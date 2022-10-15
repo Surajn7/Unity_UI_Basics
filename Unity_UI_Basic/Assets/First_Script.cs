0ba6b4f631868135bbabc8cfd9fea214d622580e
Surajn7 <105142430+Surajn7@users.noreply.github.com>
2022-10-15 23:37:17 +0530

    Scenes added

---
+++ Unity_UI_Basic/Assets/First_Script.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class First_Script : MonoBehaviour
+{
+    public string newScene;
+    public Button btn;
+    void Start()
+    {
+        btn.onClick.AddListener(Button);
+    }
+
+    private void Button()
+    {
+        Debug.Log(newScene);
+        SceneManager.LoadScene(newScene);
+    }
+
+}
